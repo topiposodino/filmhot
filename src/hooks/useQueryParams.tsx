4d0de84e8cf732185c97b51e9cd919feb8416dff
videoslaughes <93111105+videoslaughes@users.noreply.github.com>
2022-10-03 00:21:29 +0700

    ^

--- src/hooks/useQueryParams.tsx
+++
@@ -1,8 +0,0 @@
-import { useLocation } from "react-router-dom";
-
-export const useQueryParams = () => {
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-
-  return queryParams;
-};
